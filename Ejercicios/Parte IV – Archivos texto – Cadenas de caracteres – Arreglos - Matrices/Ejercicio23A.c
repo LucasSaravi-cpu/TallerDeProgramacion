33b8019fe16cd6971aea78c2555dce180d427484
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-02 11:02:56 -0300

    Update Ejercicio23A.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio23A.c
+++ Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio23A.c
@@ -1,24 +1,24 @@
-/*  contar el número de líneas en un archivo de texto.*/
+/*  contar el nÃºmero de lÃ­neas en un archivo de texto.*/
 
 #include <stdio.h>
 
 int main() {
-    FILE *file;
+    FILE *archivo;
     char line[256];
     int lines = 0;
 
-    file = fopen("archivo.txt", "r");
-    if (file == NULL) {
+    archivo= fopen("archivo.txt", "r");
+    if (archivo== NULL) {
         printf("Error al abrir el archivo\n");
         return 1;
     }
 
 
-    while (fscanf(file, "%255[^\n]\n", line) == 1) {
+    while (fscanf(archivo, "%s \n", line) == 1) {
         lines++;
     }
 
-    fclose(file);
+    fclose(archivo);
     printf("El numero de lineas del archivo es de : %d\n", lines);
     return 0;
 }
