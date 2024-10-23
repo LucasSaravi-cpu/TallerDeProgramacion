ab562440bd3199bf373fcf61d0ca046ef2e982fc
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-23 13:57:36 -0300

    Add files via upload
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

---
+++ Ejercicios/Parte V – Registros/Ejercicio36.c
@@ -0,0 +1,62 @@
+/*Ordene alfabéticamente el arreglo de Personas (utilizando el método que quiera), y luego muéstrelo.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PERSONAS 100
+
+
+typedef struct {
+    char nombre[50];
+    char sexo;
+    int edad;
+    float altura;
+} Persona;
+
+void ordenarPersonasPorNombre(Persona personas[], int n);
+
+int main() {
+    Persona personas[MAX_PERSONAS];
+    int n;
+
+     FILE *archivo = fopen("Ejercicio34.txt", "r");
+
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+       return 1;
+    }
+
+    n = 0;
+    while (fscanf(archivo, "%s %c %d %f", personas[n].nombre, &personas[n].sexo, &personas[n].edad, &personas[n].altura) != EOF) {
+        n++;
+    }
+
+    fclose(archivo);
+
+  ordenarPersonasPorNombre(personas,n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Nombre: %s, Sexo: %c, Edad: %d, Altura: %.2f\n",
+               personas[i].nombre, personas[i].sexo, personas[i].edad, personas[i].altura);
+    }
+
+
+    return 0;
+}
+
+// Función para ordenar personas por nombre utilizando el método de burbuja
+void ordenarPersonasPorNombre(Persona personas[], int n) {
+    Persona temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(personas[j].nombre, personas[j + 1].nombre) > 0) {
+                temp = personas[j];
+                personas[j] = personas[j + 1];
+                personas[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
