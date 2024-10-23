a88b69c1648bce1265dc2659426e875e74c66bf7
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-23 14:30:25 -0300

    Add files via upload
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

---
+++ Ejercicios/Parte V – Registros/Ejercicio38.c
@@ -0,0 +1,93 @@
+/* Escribir una función que permita insertar una nueva persona en el arreglo ya ordenado*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PERSONAS 100
+
+
+typedef struct {
+    char nombre[50];
+    char sexo;
+    int edad;
+    float altura;
+} Persona;
+
+void ordenarPersonasPorNombre(Persona personas[], int n);
+void insertarPersonaOrdenada(Persona personas[], int *n, Persona nuevaPersona);
+
+int main() {
+    Persona personas[MAX_PERSONAS];
+    int n;
+
+     FILE *archivo = fopen("Ejercicio34.txt", "r");
+
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+       return 1;
+    }
+
+    n = 0;
+    while (fscanf(archivo, "%s %c %d %f", personas[n].nombre, &personas[n].sexo, &personas[n].edad, &personas[n].altura) != EOF) {
+        n++;
+    }
+
+     for (int i = 0; i < n; i++) {
+        printf("Nombre: %s, Sexo: %c, Edad: %d, Altura: %.2f\n",
+               personas[i].nombre, personas[i].sexo, personas[i].edad, personas[i].altura);
+    }
+
+    fclose(archivo);
+    ordenarPersonasPorNombre(personas,n);
+
+    Persona nuevaPersona = {"Loana", 'F', 28, 1.70};
+
+    insertarPersonaOrdenada(personas, &n, nuevaPersona);
+
+    for (int i = 0; i < n; i++) {
+        printf("Nombre: %s, Sexo: %c, Edad: %d, Altura: %.2f\n",
+               personas[i].nombre, personas[i].sexo, personas[i].edad, personas[i].altura);
+    }
+
+
+    return 0;
+}
+
+
+
+void ordenarPersonasPorNombre(Persona personas[], int n) {
+    Persona temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(personas[j].nombre, personas[j + 1].nombre) > 0) {
+                temp = personas[j];
+                personas[j] = personas[j + 1];
+                personas[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void insertarPersonaOrdenada(Persona personas[], int *n, Persona nuevaPersona) {
+    if (*n >= MAX_PERSONAS) {
+        printf("No se puede insertar más personas. El arreglo está lleno.\n");
+        return;
+    }
+
+    int i = *n - 1;
+
+
+    while (i >= 0 && strcmp(personas[i].nombre, nuevaPersona.nombre) > 0) {
+        personas[i + 1] = personas[i];
+        i--;
+    }
+
+    personas[i + 1] = nuevaPersona;
+
+
+    (*n)++;
+}
+
+
+
