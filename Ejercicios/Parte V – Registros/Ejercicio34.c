e35820d20e3ffa6c0807481a2ec1e5c9f7e9ea3c
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-22 21:27:21 -0300

    Add files via upload
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

---
+++ Ejercicios/Parte V – Registros/Ejercicio34.c
@@ -0,0 +1,45 @@
+/* Modificar el ejercicio anterior para que los datos sean tomados desde un archivo de texto.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_N 100
+
+typedef struct {
+    char nombre[30];
+    int matricula;
+    char carrera[30];
+} Alumno;
+
+int main() {
+    FILE *archivo;
+    Alumno alumnos[MAX_N];
+    int n = 0;
+
+
+    archivo = fopen("ejercicio34.txt", "r");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return 1;
+    }
+
+    while (fscanf(archivo, "%29[^\n]\n%d\n%29[^\n]\n", alumnos[n].nombre, &alumnos[n].matricula, alumnos[n].carrera) != EOF) {
+        n++;
+
+    }
+
+
+    fclose(archivo);
+
+
+    printf("Alumnos de Ingenieria en Informatica:\n");
+    for (int i = 0; i < n; i++) {
+        if (strcmp(alumnos[i].carrera, "ingenieria en informatica") == 0) {
+            printf("Nombre: %s, Matricula: %d, Carrera: %s\n", alumnos[i].nombre, alumnos[i].matricula, alumnos[i].carrera);
+        }
+    }
+
+    return 0;
+}
