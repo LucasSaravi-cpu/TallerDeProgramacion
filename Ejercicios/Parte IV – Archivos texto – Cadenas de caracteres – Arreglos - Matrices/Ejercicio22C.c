0afe53ce5cbda2ecd5c74aa7c2f85cb22419a879
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-27 23:22:44 -0300

    Update Ejercicio22C.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22C.c
+++ Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22C.c
@@ -7,6 +7,7 @@ Ejemplos: 2 3 1 2 resultado 1.166 ; 4 3 2 3 resultado 2 ; 5 12 1 18 resultado 0.
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void sumarFraccionesYMostrarResultado();
 
@@ -18,15 +19,16 @@ int main() {
 void sumarFraccionesYMostrarResultado() {
     FILE *archivo;
     archivo = fopen("FRAC.TXT", "r");
+     float n1, d1, n2, d2, suma;
 
     if (archivo == NULL) {
         printf("Error al abrir el archivo para lectura.\n");
-        return;
-    }
 
-   float n1, d1, n2, d2, suma;
+    }else{
 
-    while (fscanf(archivo, "%f %f %f %f", &n1, &d1, &n2, &d2) != EOF) {
+
+
+    while (fscanf(archivo, "%f %f %f %f", &n1, &d1, &n2, &d2) ==4) {
         suma = (n1 / d1) + (n2 / d2);
         if (suma == (int)suma) {
             printf("%.0lf\n", suma);
@@ -36,4 +38,6 @@ void sumarFraccionesYMostrarResultado() {
     }
 
     fclose(archivo);
+    }
 }
+
