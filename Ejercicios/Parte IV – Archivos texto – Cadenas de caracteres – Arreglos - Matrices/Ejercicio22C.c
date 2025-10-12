d63c0c8c8dbf2227d6f0b71ce06b7678e712fc9d
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2025-10-12 13:59:02 -0300

    Update Ejercicio22C.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22C.c
+++ Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22C.c
@@ -1,43 +1,48 @@
-/*   Leer de un archivo de texto FRAC.TXT que contiene dos fracciones por línea: numerador 1,
-denominador 1, numerador 2, denominador 2. Para cada línea obtener y mostrar la suma de ambas
-fracciones, como número real con tres decimales. Si el resultado es un número entero, mostrarlo
-como tal.
-Ejemplos: 2 3 1 2 resultado 1.166 ; 4 3 2 3 resultado 2 ; 5 12 1 18 resultado 0.472  */
-
-
+/* Leer de un archivo de texto FRAC.TXT que contiene dos
+fracciones por línea: numerador 1, denominador 1, numerador 2, denominador 2. Para cada línea obtener y mostrar la suma de ambas
+fracciones, como número real con tres decimales. Si el resultado
+es un número entero, mostrarlo como tal. Ejemplos: 2 3 1 2 resultado 1.166 ; 4 3 2 3 resultado 2 ; 5 12 1 18 resultado 0.472 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> // para usar round() y fabs()
 
-void sumarFraccionesYMostrarResultado();
 
 int main() {
-    sumarFraccionesYMostrarResultado();
-    return 0;
-}
 
-void sumarFraccionesYMostrarResultado() {
-    FILE *archivo;
-    archivo = fopen("FRAC.TXT", "r");
-     float n1, d1, n2, d2, suma;
+    FILE *archivo = fopen("FRAC.TXT", "r");
 
-    if (archivo == NULL) {
+    float numerador1,numerador2,denominador1, denominador2;
+    float resultado;
+
+    if (archivo == NULL)  {
         printf("Error al abrir el archivo para lectura.\n");
 
     }else{
 
 
 
-    while (fscanf(archivo, "%f %f %f %f", &n1, &d1, &n2, &d2) ==4) {
-        suma = (n1 / d1) + (n2 / d2);
-        if (suma == (int)suma) {
-            printf("%.0lf\n", suma);
-        } else {
-            printf("%.3lf\n", suma);
-        }
+
+    while (fscanf(archivo, "%f %f %f %f", &numerador1,&denominador1,&numerador2,&denominador2) == 4) {
+
+
+       resultado = (numerador1/denominador1) + (numerador2/denominador2);
+
+
+         if (fabs(resultado - round(resultado)) < 0.0005) {
+             printf("%f / %f + %f / %f El es resultado: %.0f\n", numerador1, denominador1, numerador2, denominador2, resultado); }
+         else{
+
+            printf("%f / %f + %f / %f El es resultado: %.3f\n", numerador1, denominador1, numerador2, denominador2, resultado);}
+
+
     }
 
-    fclose(archivo);
+   fclose(archivo);
+
     }
+
+
+return 0;
+
 }
-
