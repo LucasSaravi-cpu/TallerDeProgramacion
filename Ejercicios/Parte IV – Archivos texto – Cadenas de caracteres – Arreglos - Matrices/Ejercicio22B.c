9267aa70c326c0899c05e8ac7e6dd279a8f55ca9
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2025-10-12 12:47:32 -0300

    Update Ejercicio22B.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22B.c
+++ Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22B.c
@@ -1,3 +1,50 @@
+/*Leer un archivo de texto NUMEROS.TXT que contiene un número entero en cada línea. 
+Luego grabar dos archivos de texto, uno que contenga los pares y el otro los impares */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    FILE *archivo1 = fopen("NUMEROS.TXT", "r");
+    FILE *archivo2 = fopen("PARES.TXT","w");
+    FILE *archivo3 = fopen("IMPARES.TXT","w");
+    int num;
+
+    if ((archivo1 == NULL) && (archivo2 ==NULL) && (archivo3 ==NULL))  {
+        printf("Error al abrir el archivo para lectura.\n");
+
+    }else{
+
+
+
+
+    while (fscanf(archivo1, "%d", &num) == 1) {
+
+        if (num % 2 == 0) {
+
+           fprintf(archivo2,"%d \n",num);
+
+        } else {
+
+           fprintf(archivo3,"%d \n",num);
+
+        }
+
+    }
+
+   fclose(archivo1);
+   fclose(archivo2);
+   fclose(archivo3);
+    }
+
+
+return 0;
+
+}
+
+
+
 /*   Leer un archivo de texto NUMEROS.TXT que contiene un número entero en cada línea y
 almacenar los números pares es un vector y los impares en otro  . */
 
@@ -60,4 +107,5 @@ void leerNumerosYReordenar() {
         printf("%d\n", impares[i]);
     }
 }
+
 
