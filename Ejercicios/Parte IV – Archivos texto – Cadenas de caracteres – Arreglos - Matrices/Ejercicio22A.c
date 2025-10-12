a5ac6e3bb9ea883cf3b084f7129f9f166d9e4bf2
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2025-10-12 12:26:54 -0300

    Update Ejercicio22A.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22A.c
+++ Ejercicios/Parte IV – Archivos texto – Cadenas de caracteres – Arreglos - Matrices/Ejercicio22A.c
@@ -5,49 +5,60 @@ de texto CADENAS.TXT (una por línea). Desde el archivo creado, mostrar la caden
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
+#include <stdlib.h> // Para usar el NULL
 
 int main() {
-
     FILE *archivo;
-    archivo = fopen("CADENAS.TXT", "w+");
-
     char cadena[100];
-    char cadenaMasLarga[100] ;
+    char cadenaMasLarga[100];
+
+    strcpy(cadenaMasLarga, "");
 
 
-    strcpy(cadenaMasLarga,"");
+    archivo = fopen("CADENAS.TXT", "w"); // "w" solo escritura
+
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo para escritura.\n");
+
+    }else{
+
     for (int i = 0; i < 10; i++) {
         printf("Ingrese una cadena: ");
         scanf("%s", cadena);
         fprintf(archivo, "%s\n", cadena);
     }
 
- rewind(archivo);
-
-
-   if (archivo == NULL) {
-        printf("Error al abrir el archivo para lectura.\n");
-
-    }else{
-
-  fscanf(archivo,"%s",cadena);
-     while ( !feof(archivo)) {
-        if (strlen(cadena) > strlen(cadenaMasLarga)) {
-            strcpy(cadenaMasLarga, cadena);
-        }
-
-         fscanf(archivo,"%s",cadena);
-     }
-
-       fclose(archivo);
+     fclose(archivo); // cerramos antes de leer
     }
 
 
 
 
- printf("La cadena más larga es: %s\n", cadenaMasLarga);
+    archivo = fopen("CADENAS.TXT", "r"); // "r" solo lectura
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo para lectura.\n");
 
-return 0;
+    }else{
+
+
+    fscanf(archivo,"%s",cadena);
+
+     while ( !feof(archivo)) {
+         if (strlen(cadena) > strlen(cadenaMasLarga)) {
+             strcpy(cadenaMasLarga, cadena);  }
+         fscanf(archivo,"%s",cadena);
+     }
+
+      fclose(archivo);
+     }
+
+
+
+
+
+
+
+    printf("\nLa cadena más larga es: %s\n", cadenaMasLarga);
+
+    return 0;
 }
