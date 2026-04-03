aa27473e689a339de330e00ce072de55540c88aa
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2026-04-03 16:49:19 -0300

    Update Final Taller.c
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

--- Finales/Final Taller.c
+++ Finales/Final Taller.c
@@ -1,30 +1,30 @@
 /*
 Final taller de programacion 2025 (Corte de control de primer nivel ) 
 
-Una distribuidora posee un archivo de texto con información correspondiente a las ventas de productos realizadas por distintos distribuidores.
+Una distribuidora posee un archivo de texto con informaciÃ³n correspondiente a las ventas de productos realizadas por distintos distribuidores.
 Cada registro del archivo contiene los siguientes datos:
 
-Número de distribuidor (valores entre 1 y 80, puede repetirse).
+NÃºmero de distribuidor (valores entre 1 y 80, puede repetirse).
 
-Código de producto (valores entre 100 y 299).
+CÃ³digo de producto (valores entre 100 y 299).
 
 Stock (entero positivo).
 
-Precio (número real).
+Precio (nÃºmero real).
 
 A partir de este archivo de texto se solicita realizar las siguientes tareas: 
 
-A) Hacer una sola funcion que genere un archivo binario cuyos registros sean de tipo struct y contengan la siguiente información:
+A) Hacer una sola funcion que genere un archivo binario cuyos registros sean de tipo struct y contengan la siguiente informaciÃ³n:
 
-Número de distribuidor.
+NÃºmero de distribuidor.
 
 Cantidad de unidades
 
 Promedio del precio de los productos.
 
-El archivo binario deberá generarse únicamente para aquellos distribuidores que posean más de 10 productos
+El archivo binario deberÃ¡ generarse Ãºnicamente para aquellos distribuidores que posean mÃ¡s de 10 productos
 
-Ademas Construir una matriz MS de 80x200 donde cada posición de la matriz deberá contener:
+Ademas Construir una matriz MS de 80x200 donde cada posiciÃ³n de la matriz deberÃ¡ contener:
 
 'S' si el distribuidor posee ese producto.
 
@@ -32,19 +32,189 @@ Ademas Construir una matriz MS de 80x200 donde cada posición de la matriz deberá
 
 Luego desde la matriz creada MS :
 
-B) Desarrollar una función que retorne un valor entero, que reciba como parámetro un número de distribuidor ingresado por teclado e indique si dicho distribuidor posee todos los productos de cotillón.
+B) Desarrollar una funciÃ³n que retorne un valor entero, que reciba como parÃ¡metro un nÃºmero de distribuidor ingresado por teclado e indique si dicho distribuidor posee todos los productos de cotillÃ³n.
 
-Se considera que los productos de cotillón son aquellos que cumplen con las siguientes condiciones:
+Se considera que los productos de cotillÃ³n son aquellos que cumplen con las siguientes condiciones:
 
-Código de producto entre 100 y 200 inclusive.
+CÃ³digo de producto entre 100 y 200 inclusive.
 
-Código de producto par.
+CÃ³digo de producto par.
 
-C) Desarrollar una única función que determine:
+C) Desarrollar una Ãºnica funciÃ³n que determine:
 
-El producto  que más se repite entre los distribuidores.( max)
+El producto  que mÃ¡s se repite entre los distribuidores.( max)
 
 El producto de menos se repite entre los distribuidores. ( min )
 
 D) Realizar el main completo
 */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_DIST 80
+#define MAX_PROD 200
+
+typedef struct {
+    int distribuidor;
+    int cantidadUnidades;
+    float promedioPrecio;
+} regBin;
+
+
+void procesarArchivo(char MS[][MAX_PROD]);
+void productosMaxMin(char MS[][MAX_PROD], int *prodMax, int *prodMin);
+int tieneCotillonCompleto(char MS[][MAX_PROD], int distribuidor);
+
+
+int main(){
+
+    char MS[MAX_DIST][MAX_PROD];
+
+    int distribuidor;
+    int prodMax, prodMin;
+
+    procesarArchivo(MS);
+
+    printf("Ingrese distribuidor: ");
+    scanf("%d", &distribuidor);
+
+    if(tieneCotillonCompleto(MS, distribuidor)){
+        printf("Tiene todos los productos de cotillon\n");
+    } else {
+        printf("No tiene todos los productos de cotillon\n");
+    }
+
+    productosMaxMin(MS, &prodMax, &prodMin);
+
+    printf("Producto mas distribuido: %d\n", prodMax);
+    printf("Producto menos distribuido: %d\n", prodMin);
+
+    return 0;
+}
+
+void procesarArchivo(char MS[][MAX_PROD]){
+
+    FILE *txt = fopen("ventas.txt", "r");
+    FILE *bin = fopen("salida.dat", "wb");
+
+    int dist, cod, stock;
+    float precio;
+
+    // Inicializar matriz
+    for(int i=0;i<MAX_DIST;i++)
+        for(int j=0;j<MAX_PROD;j++)
+            MS[i][j] = 'N';
+
+    if(txt == NULL || bin == NULL){
+        printf("Error al abrir archivos\n");
+
+    } else {
+
+
+    fscanf(txt, "%d %d %d %f", &dist, &cod, &stock, &precio);
+
+    while(!feof(txt)){
+
+        int distActual = dist;
+
+        int totalUnidades = 0;
+        float sumaPrecios = 0;
+        int cantProductos = 0;
+
+
+        while(!feof(txt) && dist == distActual){
+
+
+
+            int i = dist - 1;
+            int j = cod - 100;
+
+            MS[i][j] = 'S';
+
+            totalUnidades += stock;
+            sumaPrecios += precio;
+            cantProductos++;
+
+
+            fscanf(txt, "%d %d %d %f", &dist, &cod, &stock, &precio);
+
+        }
+
+
+        if(cantProductos > 10){
+
+            regBin reg;
+
+            reg.distribuidor = distActual;
+            reg.cantidadUnidades = totalUnidades;
+            reg.promedioPrecio = sumaPrecios / cantProductos;
+
+            fwrite(&reg, sizeof(regBin), 1, bin);
+        }
+    }
+
+    fclose(txt);
+    fclose(bin);
+
+    }
+}
+
+
+
+
+void productosMaxMin(char MS[][MAX_PROD], int *prodMax, int *prodMin){
+
+    int max = -1;
+    int min = 9999;
+
+    for(int j=0; j<MAX_PROD; j++){
+
+        int contador = 0;
+
+        for(int i=0; i<MAX_DIST; i++){
+            if(MS[i][j] == 'S'){
+                contador++;
+            }
+        }
+
+        if(contador > max){
+            max = contador;
+            *prodMax = j + 100;
+        }
+
+        if(contador < min && contador > 0){
+            min = contador;
+            *prodMin = j + 100;
+        }
+    }
+}
+
+int tieneCotillonCompleto(char MS[][MAX_PROD], int distribuidor){
+
+    if(distribuidor < 1 || distribuidor > MAX_DIST){
+        return 0;
+    }
+
+    int i = distribuidor - 1;
+
+    for(int cod = 100; cod <= 200; cod++){
+
+        if(cod % 2 == 0){
+
+            int j = cod - 100;
+
+            if(MS[i][j] != 'S'){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+
+
