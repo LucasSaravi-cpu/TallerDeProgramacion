66e1c13d72243b81895aafb6e3e3bd36516c56ea
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2026-05-27 22:24:13 -0300

    Add files via upload
    Signed-off-by: lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>

---
+++ Finales/Final2026.c
@@ -0,0 +1,28 @@
+/*La municipalidad de General Pueyrredón registra información de los viajes realizados por las líneas de colectivos del partido. Esta se encuentra en el archivo binario VIAJES.DAT, donde cada registro contiene los siguientes datos:
+
+Línea (500 a 594), Número de interno (1 a 80), Turno (1 a 3), Cantidad de pasajeros, KmRecorridos.
+
+Ej 1)
+
+Desarrollar en C subprogramas que a partir de los datos de VIAJES.DAT, den un único recorrido del mismo genere una matriz MP con la cantidad total de pasajeros (línea y turno) y un vector VK con los kilómetros totales por línea.
+
+Ej 2)
+
+A partir de las estructuras generadas en el ej 1:
+
+a)
+
+Crear en un subprograma un archivo de texto RESUMEN.TXT con los siguientes datos: Línea (no se repite y solo se deben incluir las líneas que hayan realizado viajes en los turnos). TotalPasajeros, TotalKm.
+y solo se deben incluir las líneas que hayan realizado viajes en los turnos.
+(Leído en el main) pasajeros entre los turnos.
+
+b)
+Hallar mediante una función y luego informar si al menos una línea ha llevado más de X pasajeros entre los tres turnos.
+
+c)
+Hallar en único subprograma para un turno T (leído y validado en el main) dado:
+
+cantidad de líneas que transportaron pasajeros en el turno T
+entre dichas líneas, cuál fue la que registró la mayor cantidad de pasajeros
+
+Escribir el main completo con la invocación a los subprogramas desarrollados. No desarrollar la carga del archivo VIAJES.DAT.*/
